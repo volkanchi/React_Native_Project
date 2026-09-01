cf9c2d3dd55a412a659ee6c4f23c1c59643a917e
Volkan Çelebi <volkan_celebii@hotmail.com>
2026-09-01 20:36:10 +0300

    cors

--- ServisTakipApi/obj/Debug/net8.0/ServisTakipApi.AssemblyInfo.cs
+++ ServisTakipApi/obj/Debug/net8.0/ServisTakipApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServisTakipApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8478021cfef12422e3ab7e726040d651f2cb2374")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30b82cdaaa01164f9fb9e2ddf01eca25dd4964dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServisTakipApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServisTakipApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
