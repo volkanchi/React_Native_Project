e2b4fc61669405bc1da2fb4cca530546420e7447
Volkan Çelebi <volkan_celebii@hotmail.com>
2026-09-02 10:38:18 +0300

    .

--- ServisTakipApi/obj/Debug/net8.0/ServisTakipApi.AssemblyInfo.cs
+++ ServisTakipApi/obj/Debug/net8.0/ServisTakipApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServisTakipApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61b3a86d4b515f375803b66358b249ffd0ba5946")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c026672d8b5ffa81a956c68bd1b40175323e1076")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServisTakipApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServisTakipApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
